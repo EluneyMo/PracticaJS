9c176a65c9a740d5b26f5a7ea0b2f7bbf123e8c4
EluneyMo <morales.eluney.epet20@gmail.com>
2022-10-04 19:05:13 -0300

    Respuesta segundo ejercicio

--- script.js
+++ script.js
@@ -7,3 +7,55 @@ console.log(nombre2);
 const nombre3="Llora";
 console.log(nombre3);
 //#endregion
+
+//#region 
+function suma(num1, num2){
+    
+    var suma=num1+num2;
+     return (suma);
+ }
+ try{
+     var num1=50;
+     var num2=30;
+     console.log(suma(num1, num2));
+ } catch {
+     const mensaje='No son numeros';
+     console.log(mensaje);
+ }
+ function resta(num1,num2){
+     var resta=num1-num2;
+     return (resta);
+ }
+ try{
+     var num1=50;
+     var num2=30;
+     console.log(resta(num1, num2));
+ } catch {
+     const mensaje='No son numeros';
+     console.log(mensaje);
+ }
+ function multi(num1,num2){
+     var multip=num1*num2;
+     return (multip);
+ }
+ try{
+     var num1=50;
+     var num2=30;
+     console.log(multi(num1, num2));
+ } catch {
+     const mensaje='No son numeros';
+     console.log(mensaje);
+ }
+ function division(num1,num2){
+     var divs=num1/num2;
+     return (divs);
+ }
+ try{
+     var num1=50;
+     var num2=5;
+     console.log(division(num1, num2));
+ } catch {
+     const mensaje='No son numeros';
+     console.log(mensaje);
+ }
+ //#endregion
